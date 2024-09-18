47c7dd2034bfbf6c81f3c05f9f91b47725fa38ac
kyungho1224 <dev.kyungho.kim@gmail.com>
2024-09-19 00:12:39 +0900

    init project

---
+++ src/components/Boards.js
@@ -0,0 +1,30 @@
+import Square from "./Square";
+import "./Boards.css";
+
+const Boards = ({ squares, onClick }) => {
+  const renderSquare = (i) => {
+    return <Square value={squares[i]} onClick={() => onClick(i)} />;
+  };
+
+  return (
+    <div className="board-wrapper">
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+export default Boards;
